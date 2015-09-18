bb7ae750d191c9932c69fc67b154971de7e0dfa8
Joseph Patterson <joseph@tsheets.com>
2015-09-18 11:14:56 -0600

    query for song instead of require id

--- application/views/home.php
+++ application/views/home.php
@@ -17,13 +17,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		$.ajax({
 			url: '/index.php/api/play_next_song'
 		}).success(function(response) {
-			var song = JSON.parse(response);
+			var songRequest = JSON.parse(response);
 			$.ajax({
-				url: 'https://api.spotify.com/v1/tracks/' + song.song_id
+				url: 'https://api.spotify.com/v1/search?q=' + songRequest.song_name + '+artist:' + songRequest.artist + '&type=track'
 			}).success(function (response) {
-				$('#song_art').prop('src', response.album.images[0].url);
+				var song = response.tracks.items[0];
+				$('#song_art').prop('src', song.album.images[0].url);
 				var player = new Audio;
-				player.src = response.preview_url;
+				player.src = song.preview_url;
 				player.play();
 			});
 		});
